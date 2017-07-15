6948c516208784d51248ddd65753a2386b09714c
Jarvis Dong <347466710@qq.com>
2017-07-15 21:30:46 +0800

    别名

--- app/src/main/java/com/example/jarvis/jiguangpush/App.java
+++ app/src/main/java/com/example/jarvis/jiguangpush/App.java
@@ -2,7 +2,10 @@ package com.example.jarvis.jiguangpush;
 
 import android.app.Application;
 
+import java.util.Set;
+
 import cn.jpush.android.api.JPushInterface;
+import cn.jpush.android.api.TagAliasCallback;
 
 /**
  * Created by jarvis on 2017/7/9.
@@ -15,5 +18,12 @@ public class App extends Application {
         JPushInterface.setDebugMode(true);
         JPushInterface.init(this);
 
+        //设置别名
+        JPushInterface.setAlias(this, "xiaoruo", new TagAliasCallback() {
+            @Override
+            public void gotResult(int i, String s, Set<String> set) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/jarvis/jiguangpush/MainActivity.java
+++ app/src/main/java/com/example/jarvis/jiguangpush/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.jarvis.jiguangpush;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cn.jpush.android.api.JPushInterface;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
